a2bdac2a61818c64ceed6739aa3183f0fa5160cb
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2020-01-01 13:32:07 -0500

    generating slug field

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,23 @@
 const axios = require('axios');
 
 const defaultFieldsToInclude = ['bibleText', 'fullTitle', 'downloadCount', 'eventType', 'media', 'preachDate', 'series', 'speaker'];
-const requiredFields = ['id', 'internal'];
-const defaultOptions = { fieldsToInclude: defaultFieldsToInclude };
+const requiredFields = ['id', 'internal', 'slug'];
+const defaultOptions = { fieldsToInclude: defaultFieldsToInclude, slugField: 'fullTitle' };
 
-const processSermons = (sermon, fieldsToInclude = defaultFieldsToInclude) => {
+const getSlug = (sermon, slugField) => {
+    return sermon[slugField].split(" ").map(word => word.toLowerCase()).join("-");
+};
+
+const processSermons = (sermon, fieldsToInclude = defaultFieldsToInclude, slugField) => {
     return Object.keys(sermon)
         .reduce((acc, key) => {
             if (fieldsToInclude.concat(requiredFields).includes(key)) {
+                if (key === 'slug') {
+                    return {
+                        ...acc,
+                        slug: getSlug(sermon, slugField)
+                    };
+                }
                 return {
                     ...acc,
                     [key]: sermon[key]
@@ -33,7 +43,7 @@ const getSermons = (apiKey, query = '/v2/node/sermons', acc = []) => {
 }
 
 exports.sourceNodes = async ({ actions }, options = defaultOptions) => {
-    const { apiKey, fieldsToInclude } = { ...defaultOptions, ...options };
+    const { apiKey, fieldsToInclude, slugField } = { ...defaultOptions, ...options };
     const { createNode } = actions;
 
     // getting the list of items for calendar
@@ -49,7 +59,7 @@ exports.sourceNodes = async ({ actions }, options = defaultOptions) => {
                 type: 'Sermon'
             }
         }))
-        .forEach(sermon => createNode(processSermons(sermon, fieldsToInclude)));
+        .forEach(sermon => createNode(processSermons(sermon, fieldsToInclude, slugField)));
   
     // We're done, return.
     return;
